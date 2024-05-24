e1f52f3652dd8c0dce20371ae064404e83923250
Horad <iranradmard1@gmail.com>
2024-05-24 23:38:16 +0330

    in progress

--- TAF/urls.py
+++ TAF/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tables/', include('tables.urls')),
+    # path('tables/', include('tables.urls')),
+    path('Database/', include('Database.urls')),
 ]
--- tables/models.py
+++ tables/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Professor(models.Model):
     prof_id = models.CharField(primary_key = True, unique = True)
-    # use_id = models.CharField(primary_key = True, unique = True)
-    # dept_id = models.CharField(primary_key = True, unique = True)
+    use_id = models.CharField(primary_key = True, unique = True)
+    dept_id = models.CharField(primary_key = True, unique = True)
     rank = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -11,6 +11,7 @@ class Professor(models.Model):
     
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+    
 class Department(models.Model):
     dept_id = models.CharField(primary_key = True, unique = True)
     dept_name = models.CharField(max_length=50)
