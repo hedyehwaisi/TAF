4fcac29c4b9550f5441e9e2ad869182f19613d33
Horad <iranradmard1@gmail.com>
2024-05-24 23:46:37 +0330

    in progress

--- Database/urls.py
+++ Database/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('Database/', views.members, name='members'),
 ]
--- TAF/settings.py
+++ TAF/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'tables',
+    # 'users',
+    # 'tables',
     'Database',
 ]
 
