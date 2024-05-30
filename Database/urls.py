286c21dba929a3b9f5953a4615236320f625384e
Horad <iranradmard1@gmail.com>
2024-05-30 09:23:20 +0330

    in progress

--- Database/urls.py
+++ Database/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Database/', views.members, name='members'),
+    # path('Database/', views.members, name='members'),
+    path('', views.members, name='members'),
 ]
